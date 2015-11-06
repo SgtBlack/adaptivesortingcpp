6c3e47185084e24738feafc2f54a34d4a06f949a
Fabian <fabian.witt@t-online.de>
2015-11-06 19:15:51 +0100

    basic c++ files

---
+++ adaptivesorting/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Fabian Witt
+ *
+ * Created on 6. November 2015, 19:14
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
